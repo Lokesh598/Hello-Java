e001e8301b23bbf690ae962b996498490e08a4c0
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-05-08 12:49:01 +0530

    Create print char array.java

---
+++ Array/print char array.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class solution {
+    public static void main(String[] args) {
+
+        char[] str ={'A','B','C'} ;
+        System.out.println(str);
+    }
+}
