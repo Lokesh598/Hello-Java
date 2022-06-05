24c60f4b79d86dab94662675419a9746094edddf
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-06-06 01:50:53 +0530

    Create Second.java

---
+++ JAVA-OOPS/Classes/multipleClasses/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+  public static void main(String[] args) {
+    Main obj = new Main();
+    
+    System.out.println("Value of x is" + obj.x); // 5
+  }
+}
+
+// object created for main class to beacuse we want to access the members of main class in second class.
